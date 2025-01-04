cf21b2c6784457d3113915003f3712f7f556ccde
brejlpat <patrikbrejla@seznam.cz>
2025-01-04 23:29:30 +0100

    FlaskApp v.2 - aktuality, formulare

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-# Nastavení e-mailu pro Seznam
-SMTP_SERVER = "smtp.seznam.cz"  # SMTP server Seznam.cz
-SMTP_PORT = 587  # SMTP port (pro TLS)
-EMAIL_ADDRESS = "webtest.mail@seznam.cz"  # Nahraď svým e-mailem na Seznam.cz
-EMAIL_PASSWORD = "Web_test123"  # Nahraď svým heslem (nebo použij heslo aplikace)
 
 
 @app.route("/")
@@ -24,3 +15,4 @@ def formulare():
 
 if __name__ == "__main__":
     app.run()
+    #app.run(debug=True, port=5000, host="0.0.0.0")
